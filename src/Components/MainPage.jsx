e15eda2080e8577f18eec49da3676b60d0ad1ea2
Aram Krakirian <aramkrakirian@gmail.com>
2022-01-26 10:12:44 -0800

    frontend UI & functionality almost done

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -3,10 +3,7 @@ import { useState } from 'react';
 import FlashcardContainer from './FlashcardContainer.jsx';
 
 const MainPage = () => {
-
-  const openForm = () => {
-    
-  };
+  const openForm = () => {};
 
   const closeForm = () => {
     document.getElementById('myForm').style.display = 'none';
@@ -27,65 +24,81 @@ const MainPage = () => {
       .then((data) => console.log(data))
       .catch((err) => console.log(err));
     e.preventDefault();
-  }
+  };
 
   return (
     <div className="header">
       <h1 className="main">Time to Study ALGOS</h1>
-      <div className="form-popup" id="myForm">
-        <form
-          className="form-container"
-          id="form-container"
-          onSubmit={onSubmitClick}
-        >
-          <h1>Create flashcard</h1>
-          <label htmlFor="algoName">
-            <b>Algorithm Name</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Algorithm Name"
-            name="algoName"
-            id="algoName"
-          />
-          <label htmlFor="algoPrompt">
-            <b>Algorithm Prompt</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Algorithm Prompt"
-            name="algoPrompt"
-            id="algoPrompt"
-          />
-          <label htmlFor="algoExample">
-            <b>Algorithm Example</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Algorithm Example"
-            name="algoExample"
-            id="algoExample"
-          />
-          <label htmlFor="algoType">
-            <b>Algorithm Type</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Algorithm Type"
-            name="algoType"
-            id="algoType"
-          />
-          <button type="submit" className="btn btn-primary">
-            Create
-          </button>
-          <button type="reset" className="btn btn-cancel" onClick={closeForm}>
-            Discard
-          </button>
-        </form>
+      <div className="modal" id="create">
+        <div className="modal-content">
+          <a href="#close" className="close-link">
+            X
+          </a>
+          <form
+            className="form-container"
+            id="form-container"
+            onSubmit={onSubmitClick}
+          >
+            <h1>Create flashcard</h1>
+            <label htmlFor="algoName">
+              <b>Algorithm Name</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Algorithm Name"
+              name="algoName"
+              id="algoName"
+            />
+            <label htmlFor="algoPrompt">
+              <b>Algorithm Prompt</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Algorithm Prompt"
+              name="algoPrompt"
+              id="algoPrompt"
+            />
+            <label htmlFor="algoExample">
+              <b>Algorithm Example</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Algorithm Example"
+              name="algoExample"
+              id="algoExample"
+            />
+            <label htmlFor="algoType">
+              <b>Algorithm Type</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Algorithm Type"
+              name="algoType"
+              id="algoType"
+            />
+            <button type="reset" className="btn btn-cancel popup-btn">
+              Reset
+            </button>
+            <button type="submit" className="btn btn-primary popup-btn">
+              Create
+            </button>
+          </form>
+        </div>
+        <a href="#close" className="background"></a>
       </div>
-      <button className="btn btn-primary btn-large" type="button" onClick={openForm}>
+      <a className="btn btn-primary btn-large" href="#create">
         Create a new flashcard
-      </button><br/><br/>
+      </a>
+      {/* <button
+        className="btn btn-primary btn-large"
+        href="#create"
+        type="button"
+        onClick={openForm}
+      >
+        Create a new flashcard
+      </button> */}
+      <br />
+      <br />
       <FlashcardContainer />
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import MainPage from "./Components/MainPage.jsx";
-import "./styles/style.css";
+import "./styles/style.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
